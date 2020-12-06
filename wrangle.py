6481ece9e9538366970557a6050bb00c5f0a2891
datastraine <anthony.straine@gmail.com>
2020-12-06 14:57:02 -0600

    adds political score, full_ad_length column, and full_ad columns. Also fixes a few bugs.

--- wrangle.py
+++ wrangle.py
@@ -58,7 +58,6 @@ def remove_stopwords(string, keep_words=['no', 'not'], exclude_words=[]):
     string_without_stopwords = ' '.join(filtered_words)
     return string_without_stopwords
 
-
 def wrangle(facebook_data_csv_location):
     '''
     This function requires that you download and unzip the file from the link below. File size is over 3gigs
@@ -66,33 +65,63 @@ def wrangle(facebook_data_csv_location):
     Once unzipped, use the file path in the fuction to:
     * Load the csv into a pandas dataframe
     * Drops the following columns - 'listbuilding_fundraising_proba', 'targetedness', 'targetings', 'targets', 
-    'targeting', 'images', 'lang', 'thumbnail', 'html', 'page_id', 'suppressed', 'entities', 'lower_page'
-    * Create a socio_political_fb column that is True if facebook believes that the ad is about soical or political topics, 
-    and false if facebook does not
+    'targeting', 'images', 'lang', 'thumbnail', 'html', 'page_id', 'suppressed', 'entities', 'lower_page', 'id', and 'political_probability'
+    * Create a socio_political_fb column that is 'Political' if facebook believes that the ad is about soical or political topics (advert_by is not null), 
+    and 'Not Political' if facebook does not (advert_by is null)
     * Combine the advertiser and paid_for_by columns, with a priority given to paid_for_by values into a advert_by column
     * If advert_by column is null and the page is known, replace the null value with the page value
     * Fills remaining null values with 'unk' for advert_by column
     * Drops any row that has a missing title value (47)
     * Drops the remaining columns with null 
-    * Normalizes the message columns by conducting basic_clean, tokenization, removing stop words, and lemmatizing the text
+    * Normalizes the message and title columns by conducting basic_clean, tokenization, removing stop words, and lemmatizing the text
+    * Transforms 
+    * Creates a known_ad_time column which is when the ad was last updated minus when the ad was first seen
+    * Combines the message and title columns into a full_ad column 
+    * Creates a known_ad_time column that is the difference between the updated_at and created_at, and adds 1 to result to account for 0 day
     * Splits the data into train, validate, and test sets that are stratified on socio_political_fb
+    * Creates message and full_ad lengths columns by counting the length of a list created using .str.split() to create a list of words
+    * Creates a political_score column by subtracting political from not_political. Negative rating happens when users said 
+    an ad was not_political more than political and the opposite for positive numbres
     * returns train, validate, test
     '''
-
+    # read the data in a df
     df = pd.read_csv(facebook_data_csv_location)
+    # drops unnecessary colunms
     df.drop(columns=['listbuilding_fundraising_proba', 'targetedness', 'targetings', 'targets', 
-    'targeting', 'images', 'lang', 'thumbnail', 'html', 'page_id', 'suppressed', 'entities', 'lower_page'], inplace=True)
+    'targeting', 'images', 'lang', 'thumbnail', 'html', 'page_id', 'suppressed', 'entities', 'lower_page', 'id', 'political_probability'], inplace=True)
+    # creates the  socio_political_fb column
     df['socio_political_fb'] = df['paid_for_by'].notnull()
+    df['socio_political_fb'] = df['socio_political_fb'].replace({True:'Political', False:'Not Political'})
+    # combines the paid_for_by and advertized by columns into a advert_by column
     df['advert_by'] = np.where(df.paid_for_by.isnull(), df.advertiser, df.paid_for_by)
     df.advert_by = np.where(df.advert_by.isnull(), df.page, df.advert_by)
     df['advert_by'].fillna('unk', inplace=True)
+    # Drops any row where title is null
     df.dropna(subset=['title'], inplace= True)
+    # Drops remaining columns with null values 
     df.dropna(axis=1, inplace=True)
+    # Removes html font formatting and cleans the message and title texts
     df['message'] = df['message'].str.replace(r'(<.+\")', "")
     df['message'] = df['message'].str.replace("</span>", "")
     df['message'] = df['message'].str.replace("</p>", "")
     df['message'] = df['message'].str.replace("<p>", "")
     df['message'] = df['message'].apply(basic_clean).apply(tokenize).apply(remove_stopwords).apply(lemmatize)
+    df['title'] = df['title'].apply(basic_clean).apply(tokenize).apply(remove_stopwords).apply(lemmatize)
+    # converts the created_at and updated_at columns to datatime objs
+    df['created_at'] = pd.to_datetime(df['created_at'])
+    df['updated_at'] = pd.to_datetime(df['updated_at'])
+    # creates a known_ad_time column and adds 1 to results to account for 0 day
+    df['known_ad_time'] = df['updated_at'] - df['created_at'] 
+    df['known_ad_time'] = df['known_ad_time'] // pd.Timedelta('1d') 
+    df['known_ad_time'] = df['known_ad_time'] + 1
+    # creates a message length column that is the total number of words of the ad's message
+    df['message_length'] = df['message'].str.split().str.len()
+    # creates a full_ad column that combines the words from the title and the ad message into a single column
+    df['full_ad'] = df['message'] + " " + df['title']
+    # creates a full_ad_length that counts the number of words in the full_ad
+    df['full_ad_length'] = df['full_ad'].str.split().str.len()
+    df['political_score'] = df['political'] - df['not_political']
+    # splits the data into train, validate, test sets and stratifies on socio_political_fb
     train_validate, test = train_test_split(df, 
                                         stratify=df.socio_political_fb, 
                                         test_size=.2, 
